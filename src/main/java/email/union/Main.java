113019d0b9c013cf73a75dc32e921d5d4273c84a
lihuan7 <svnikitin@diasoft.ru>
2018-11-25 21:13:08 +0500

    add find user

--- src/main/java/email/union/Main.java
+++ src/main/java/email/union/Main.java
@@ -5,7 +5,8 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    private static Map<String, List<String>> usersMap = new HashMap<>();
+    //private static Map<String, List<String>> usersMap = new HashMap<>();
+    private static Map<String, List<String>> resultMap = new HashMap<>();
     private static Map<String, String> emailMap = new HashMap<>();
 
     public static void main(String[] args) {
@@ -19,32 +20,47 @@ public class Main {
         final String listStr = userStr.substring(userStr.lastIndexOf("->") + 2).trim();
         final List<String> listEmail = Arrays.asList(listStr.trim().split("\\s*,\\s*"));
 
-        System.out.println("key :" + key);
-        System.out.println("listStr :" + listStr);
+        //System.out.println("key :" + key);
+        //System.out.println("listStr :" + listStr);
         //System.out.println("listEmail :" + listEmail.p);
+
+         String foundUser=null;
+
         for (String email : listEmail) {
-            if (emailMap.get(email) == null)
-                emailMap.put(email, key);
+            if (emailMap.get(email) != null) {
+                foundUser = emailMap.get(email);
+            }
         }
-        //usersMap.put(key, listEmail);
+
+        if (foundUser!=null){
+            for (String email : listEmail) {
+                emailMap.put(email,foundUser);
+            }
+        }else {
+            for (String email : listEmail) {
+                emailMap.put(email,key);
+            }
+        }
+
+        System.out.println("--");
     }
 
     public static void getUsersEmails(){
         for(Map.Entry<String,String> entry : emailMap.entrySet()){
-            if (usersMap.get(entry.getValue())==null){
+            if (resultMap.get(entry.getValue())==null){
                 List<String> emails = new ArrayList<String>();
                 emails.add(entry.getKey());
-                usersMap.put(entry.getValue(),emails);
+                resultMap.put(entry.getValue(),emails);
             }else{
-                List<String> emails = usersMap.get(entry.getValue());
+                List<String> emails = resultMap.get(entry.getValue());
                 emails.add(entry.getKey());
-                usersMap.put(entry.getValue(),emails);
+                resultMap.put(entry.getValue(),emails);
             }
         }
     }
 
     public static void printResults(){
-        for( Map.Entry<String,List<String>> listEntry : usersMap.entrySet()){
+        for( Map.Entry<String,List<String>> listEntry : resultMap.entrySet()){
             System.out.println(listEntry.getKey() + "->" + listEntry.getValue().stream()
                     .collect(Collectors.joining(",")));
         }
